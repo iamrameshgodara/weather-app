c1d47075545e9562107a3b24cdd454613e126b72
Ramesh Godara <rameshgodara@Rameshs-MacBook-Air.local>
2020-10-02 13:50:32 +0530

    added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
 
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. And Humidity is ' + body.current.humidity + '%.')
+
         }
 
     })
